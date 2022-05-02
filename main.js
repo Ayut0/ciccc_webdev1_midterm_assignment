a8f5af0451f3e7beb98c1e6b494da744d50589ff
Yuto Yamakita <yuvto.0420@gmail.com>
2022-05-01 20:32:11 -0700

    Create a dark mode style

---
+++ main.js
@@ -0,0 +1,36 @@
+window.addEventListener("DOMContentLoaded", ()=>{
+    let toggleBtn = document.querySelector("#modeSwitchBtn");
+    const aboutMeSection = document.querySelector("#aboutMeSection");
+    const skillMeSection = document.querySelector("#skillsSection");
+    const projectMeSection = document.querySelector("#projectsSection");
+    const qualificationMeSection = document.querySelector("#qualificationSection");
+    const contactMeSection = document.querySelector("#contactSection");
+    const footer = document.querySelector(".footer");
+    const boxTitle = document.querySelectorAll(".box-title");
+
+    const introSentence = document.querySelector(".intro-sentence");
+    const logo = document.querySelectorAll("#logo");
+    const words = document.querySelectorAll("#word");
+
+    const switchTheme = () =>{
+        toggleBtn.classList.toggle("fa-sun")
+        aboutMeSection.classList.toggle("darkMode");
+        skillMeSection.classList.toggle("darkMode");
+        projectMeSection.classList.toggle("darkMode");
+        qualificationMeSection.classList.toggle("darkMode");
+        contactMeSection.classList.toggle("darkMode");
+        footer.classList.toggle("darkMode");
+        introSentence.classList.toggle("darkMode");
+        logo.forEach(element => {
+            element.classList.toggle("darkMode");
+        });
+        words.forEach(word => {
+            word.classList.toggle("darkMode");
+        });
+        boxTitle.forEach(item => {
+            item.classList.toggle("darkMode");
+        });
+    }
+
+    toggleBtn.addEventListener("click", switchTheme);
+})
